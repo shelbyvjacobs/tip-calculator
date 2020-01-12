c9de61afb5a3fe12b9564ece3bfa1e4c8c0a648d
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-12 11:19:45 -0500

    pseudocoded javascript

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test");
+// Pseudocode
+
+//collect values from inputs (bill total, service quality, and number of people)
+//validate inputs
+    //make sure nothing is empty or unchecked
+//calculate based on collected values
+    //bill total * service quality / number of people
+    //round the total to hundreth place
+//innerHTML for displaying tip
+//click submit to call calculation function
