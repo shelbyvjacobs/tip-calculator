3cf2e5c33a761d7839286fdd31fcf3272540078f
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-14 16:15:19 -0500

    added removeTip function so that on reset, the page returns to default

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 submitButton = document.querySelector("#submit");
+resetButton = document.querySelector("#reset");
 tipSection = document.querySelector("#tip-section");
 tipAmount = document.querySelector("#tip-amount");
 
 submitButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     calculateTip();
+})
+
+resetButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    removeTip();
 })
 
 calculateTip = () => {
@@ -23,3 +29,13 @@ displayTip = () => {
     tipSection.style.display ="block";
     tipAmount.innerText = "$" + roundedTip;
 };
+
+removeTip = () => {
+    tipSection.style.display = "none";
+    billTotal = document.querySelector("#bill-total").value = "";
+    serviceQuality = document.querySelector("#service-quality").value = "";
+    numberOfPeople = document.querySelector("#number-of-people").value = "";
+};
+
+// add validation to make sure all fields are completed.
+// make sure the bill total is a number!
